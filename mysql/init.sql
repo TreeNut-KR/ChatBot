d607fe838d66735fe9cdecc4223282fabc6ec607
jgkim14 <48284333+jgkim14@users.noreply.github.com>
2024-08-01 20:40:24 +0900

    올라간 디비파일 삭제

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,4 +1,42 @@
 CREATE DATABASE IF NOT EXISTS chatbot;
 
 
+-- 유저
+CREATE TABLE users (
+    idx INT AUTO_INCREMENT,
+    id VARCHAR(50),
+    username VARCHAR(50),
+    email VARCHAR(100),
+    password VARCHAR(255),
+    access_token text,
+    refresh_token text,
+    created_at DATETIME DEFAULT NOW(),
+    updated_at DATETIME DEFAULT NOW(),
+    PRIMARY KEY(idx)
+) ENGINE=InnoDB CHARSET=utf8mb4;
+
+-- 캐릭터
+CREATE TABLE characters (
+    characters_pk INT AUTO_INCREMENT,
+    name VARCHAR(30),
+    created_at DATETIME DEFAULT NOW(),
+    updated_at DATETIME DEFAULT NOW(),
+    PRIMARY KEY(characters_pk)
+) ENGINE=InnoDB CHARSET=utf8mb4;
+
+
+-- 채팅방
+CREATE TABLE chatroom (
+    chatroom_pk INT AUTO_INCREMENT,
+    users_idx INT,
+    characters_pk INT,
+    mongo_chatlog VARCHAR(100),
+    created_at DATETIME DEFAULT NOW(),
+    updated_at DATETIME DEFAULT NOW(),
+    PRIMARY KEY(chatroom_pk),
+    FOREIGN KEY (users_idx) REFERENCES users(idx), /*외부키 설정*/
+    FOREIGN KEY (characters_pk) REFERENCES characters(characters_pk) /*외부키 설정*/
+) ENGINE=InnoDB CHARSET=utf8mb4;
+
+
 USE chatbot;
