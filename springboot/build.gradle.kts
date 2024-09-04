77eb9c4c8522dc7ce96277e48c81d76c40bc384f
jgkim14 <48284333+jgkim14@users.noreply.github.com>
2024-09-04 16:08:05 +0900

    의존성 문제 해결

--- springboot/build.gradle.kts
+++ springboot/build.gradle.kts
@@ -24,15 +24,20 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
 
-    runtimeOnly("mysql:mysql-connector-java:8.0.33") // 버전 추가
+    runtimeOnly("mysql:mysql-connector-java:8.0.33") // MySQL Connector 버전
     implementation("io.jsonwebtoken:jjwt:0.9.1")
     implementation("javax.servlet:javax.servlet-api:4.0.1")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    
+    // JAXB 관련 의존성 추가
+    implementation("javax.xml.bind:jaxb-api:2.3.1")
+    implementation("org.glassfish.jaxb:jaxb-runtime:2.3.1")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
+
 
 tasks.withType<Test> {
     useJUnitPlatform()
--- springboot/src/main/java/com/TreeNut/ChatBot_Backend/config/SecurityConfig.kt
+++ springboot/src/main/java/com/TreeNut/ChatBot_Backend/config/SecurityConfig.kt
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class CorsConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 설정
-            .allowedOrigins("http://localhost:3000") // 허용할 출처 (예: React 앱)
+            .allowedOrigins("http://localhost:80") // 허용할 출처 (예: React 앱)
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
             .allowedHeaders("*") // 허용할 헤더
             .allowCredentials(true) // 인증 정보를 포함할 수 있도록 설정
