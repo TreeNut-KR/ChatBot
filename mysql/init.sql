3d70ca9bb4542377231bd76a3056edb606907fe6
jgkim14 <48284333+jgkim14@users.noreply.github.com>
2024-09-09 15:56:48 +0900

    Update init.sql

--- mysql/init.sql
+++ mysql/init.sql
@@ -3,39 +3,37 @@ FLUSH PRIVILEGES;
 
 CREATE DATABASE IF NOT EXISTS chatbot;
 
-
 -- 유저
 CREATE TABLE users (
     idx INT AUTO_INCREMENT,
-    idx INT AUTO_INCREMENT,
-    userid VARCHAR(50),
+    userid VARCHAR(50) UNIQUE,
     username VARCHAR(50),
     email VARCHAR(100),
     password VARCHAR(255),
-    access_token text,
-    refresh_token text,
-    created_at DATETIME DEFAULT NOW(),
-    updated_at DATETIME DEFAULT NOW(),
-    PRIMARY KEY(idx)
+    access_token TEXT,
+    refresh_token TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY(idx)
 ) ENGINE=InnoDB CHARSET=utf8mb4;
 
+
 -- 캐릭터
 CREATE TABLE characters (
-    
     idx INT AUTO_INCREMENT,
-    uuid CHAR(36) UNIQUE DEFAULT UUID(),
-    useridx VARCHAR(50),
-    character_name VARCHAR(30),
+    uuid CHAR(36) UNIQUE NOT NULL,
+    useridx INT, 
+    character_name VARCHAR(30) NOT NULL,
     character_setting VARCHAR(255),
     description VARCHAR(255),
     greeting TEXT,
-    accesslevel int,
-    created_at DATETIME DEFAULT NOW(),
-    updated_at DATETIME DEFAULT NOW(),
-    PRIMARY KEY(characters_pk),
-    FOREIGN KEY (creater) REFERENCES users(idx)
+    accesslevel INT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY(idx),
+    FOREIGN KEY (useridx) REFERENCES users(idx)
 ) ENGINE=InnoDB CHARSET=utf8mb4;
+
 
 
 
