8f428ff39e82f6712395e25f7a0547a5a4d90568
SeoJungHun <sjmbee04@gmail.com>
2024-09-19 14:09:18 +0900

    누락된 config/SecurityConfig.kt  수정

---
+++ springboot/src/main/java/com/TreeNut/ChatBot_Backend/controller/config/SecurityConfig.kt
@@ -0,0 +1,17 @@
+package com.TreeNut.ChatBot_Backend.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**") // 모든 경로에 대해 CORS 설정
+            .allowedOrigins("http://localhost:80") // 허용할 출처 (예: React 앱)
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+            .allowedHeaders("*") // 허용할 헤더
+            .allowCredentials(true) // 인증 정보를 포함할 수 있도록 설정
+    }
+}
