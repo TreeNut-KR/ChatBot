c714ed8de2808873ad449fd8c6c706040965faf7
donismoney <jaehyun2112@naver.com>
2025-01-12 16:10:01 +0900

    병합 일부 수동 조합

--- springboot/src/main/java/com/TreeNut/ChatBot_Backend/controller/config/SecurityConfig.kt
+++ springboot/src/main/java/com/TreeNut/ChatBot_Backend/controller/config/SecurityConfig.kt
@@ -12,10 +12,29 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableWebSecurity
 class SecurityConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
+
         registry.addMapping("/**") // 모든 경로에 대해 CORS 설정
-            .allowedOrigins("http://localhost:80") // 허용할 출처 (예: React 앱)
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
-            .allowedHeaders("*") // 허용할 헤더
-            .allowCredentials(true) // 인증 정보를 포함할 수 있도록 설정
+            .allowedOrigins(
+                "http://localhost:80", //정식 런칭 시 사용 도메인으로 변경
+                "http://localhost:8080",
+                "http://localhost:3000" //정식 런칭 시 삭제
+            )
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+    }
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf().disable()
+            .cors().and()
+            .authorizeHttpRequests { auth ->
+                auth
+                    .requestMatchers("/server/user/**", "/oauth2/**", "/oauth/callback/kakao").permitAll()
+                    .anyRequest().authenticated()
+            }
+            .oauth2Login() // OAuth2 로그인 활성화
+        return http.build()
     }
 }
