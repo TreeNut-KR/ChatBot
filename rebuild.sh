eda2f9c465daad1c2b9197ebbed94e1ccd5333b2
mhj228 <mhj2281@naver.com>
2025-03-07 16:51:13 +0900

    젠킨스 테스트 커밋

--- rebuild.sh
+++ rebuild.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 # 모든 컨테이너 중지
-docker stop $(docker ps -aq)
+if [ "$(docker ps -aq)" ]; then
+    docker stop $(docker ps -aq)
+fi
 
 # 모든 컨테이너 삭제
-docker rm $(docker ps -aq)
+if [ "$(docker ps -aq)" ]; then
+    docker rm $(docker ps -aq)
+fi
 
 # 모든 이미지 삭제
-docker rmi $(docker images -q)
+if [ "$(docker images -q)" ]; then
+    docker rmi $(docker images -q)
+fi
 
-#리빌드
+# 리빌드
 docker compose up -d
 
 echo "도커 컨테이너 리빌드 완료"
