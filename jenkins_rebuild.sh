5339bf0f284c2e2209c8ec7d6fba887d697f0a44
qwer679 <91dlwnsgh@naver.com>
2025-03-21 16:24:01 +0900

    변경1

--- jenkins_rebuild.sh
+++ jenkins_rebuild.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-#모든 컨테이너 중지
-if ["$(docker ps -aq)"]; then
+# 모든 컨테이너 중지
+if [ "$(docker ps -aq)" ]; then
     docker stop $(docker ps -aq)
 fi
 
-#모든 컨테이너 삭제
-if ["$(docker ps -aq)"]; then
+# 모든 컨테이너 삭제
+if [ "$(docker ps -aq)" ]; then
     docker rm $(docker ps -aq)
 fi
 
-#모든 이미지 삭제
-if ["$(docker images -aq)"]; then
+# 모든 이미지 삭제
+if [ "$(docker images -aq)" ]; then
     docker rmi $(docker images -aq)
 fi
 
-#이미지 빌드
+# 이미지 빌드
 docker-compose up -d --build
 
 echo "jenkins_rebuild.sh is done"
