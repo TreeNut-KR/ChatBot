82a512c7b8e087317704b7ba57d7e3f5a1adc8d5
jgkim14 <jgkim142@naver.com>
2025-04-07 17:33:34 +0900

    어드민 관리 컨테이너 기반 파일 추가

---
+++ admincontroller/client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>TreeNut Admin</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
